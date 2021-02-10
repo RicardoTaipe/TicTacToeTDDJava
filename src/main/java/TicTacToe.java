ceac99e4e36d7b1406fdec589fe054037523bd48
Ricardo Taipe <ricardo.taipe@devsu.com>
2021-02-10 12:13:35 -0500

    First test and refactoring
    Checking axis on tictactoe board

---
+++ src/main/java/TicTacToe.java
@@ -0,0 +1,24 @@
+public class TicTacToe {
+    private Character[][] board = {{'\0', '\0', '\0'}, {'\0', '\0', '\0'}, {'\0', '\0', '\0'}};
+
+    public void play(int x, int y) {
+        checkAxis(x);
+        checkAxis(y);
+        setBox(x,y);
+    }
+
+    public void setBox(int x ,int y){
+        if(board[x-1][y-1]!= '\0'){
+            throw new RuntimeException("Box is occupied");
+        }else{
+            board[x-1][y-1]='X';
+        }
+    }
+
+    public void checkAxis(int axis){
+        if (axis < 1 || axis > 3) {
+            //X represents the place not the Axis x
+            throw new RuntimeException("X is outside the board");
+        }
+    }
+}
---
+++ src/test/java/TicTacToeSpec.java
@@ -0,0 +1,30 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TicTacToeSpec {
+    private TicTacToe ticTacToe;
+
+    @BeforeEach
+    public final void before(){
+        ticTacToe = new TicTacToe();
+    }
+
+    @Test
+    public void whenXOutsideBoardThenRuntimeException(){
+        assertThrows(RuntimeException.class, ()-> ticTacToe.play(5,2));
+    }
+
+    @Test
+    public void whenYOutsideBoardThenRuntimeException(){
+        assertThrows(RuntimeException.class, ()-> ticTacToe.play(2,5));
+    }
+
+    @Test
+    public void whenOccupiedPlaceThenRuntimeException(){
+        ticTacToe.play(2,1);
+        assertThrows(RuntimeException.class, ()-> ticTacToe.play(2,1));
+    }
+
+}
